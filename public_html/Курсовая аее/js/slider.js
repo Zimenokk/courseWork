bda1102081945f37fdd4b1cfc4bbcccb0c4e11dd
Zimenokk <dzimenokk@gmail.com>
2020-12-07 02:27:57 +0200

    1

--- public_html/Курсовая аее/js/slider.js
+++ public_html/Курсовая аее/js/slider.js
@@ -27,3 +27,15 @@ new Swiper('.image-slider', {
 
 });
 // Скролл
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+}
